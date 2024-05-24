b73da2c9a637f2fcf54610e4e3a1cf5796b0730b
Av <yungvade1@gmail.com>
2024-05-24 15:12:58 -0700

    cors

--- src/server.ts
+++ src/server.ts
@@ -12,13 +12,25 @@ const port = process.env.PORT || 4001;
 
 const app = express();
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
-app.use(
+{/*app.use(
     cors({
     origin: "http://localhost:3000",
     credentials: true,
 })
-);
+);*/}
+
+
+app.use(cors({
+    origin: 'https://urlshtnr.vercel.app/', // use your actual domain name (or localhost), using * is not recommended
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true
+}))
+
+
+
 
 app.use("/api/", shortUrl);
 
