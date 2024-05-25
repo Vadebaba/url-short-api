0669e72afbe1cac664cc67b074c6ae9d3f524686
Av <yungvade1@gmail.com>
2024-05-25 00:24:25 -0700

    cors

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://urlshtnr.vercel.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
